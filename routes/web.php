73fb59ad3ce9461e2e1999859f0866b9098a5a5f
alfreding0 <alfredogonzales953@gmail.com>
2022-01-12 01:00:00 -0400

    deshabilitado vista welcome y vista register

--- routes/web.php
+++ routes/web.php
@@ -16,41 +16,45 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
+
+//redireccionando para deshabiliar welcome y register
+Route::get('/', function () {
+    return redirect()->route('home');
+});
+Route::get('register', function(){
+    return redirect()->route('home');
+});
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::group(['middleware'=>'auth'], function(){
     Route::group(['prefix'=>'personal'], function (){
-        Route::get('/', [PersonalController::class, 'index'])->name('personal.index');  //127.0.0.1:8000/personal/
-        Route::get('/create', [PersonalController::class, 'create'])->name('personal.create');  //127.0.0.1:8000/personal/create
-        Route::post('/', [PersonalController::class, 'store'])->name('personal.store');  //127.0.0.1:8000/personal/
-        Route::get('/{id}', [PersonalController::class, 'show'])->name('personal.show'); //127.0.0.1:8000/personal/83
-        Route::get('/{id}/edit', [PersonalController::class, 'edit'])->name('personal.edit'); //127.0.0.1:8000/personal/83/edit
-        Route::put('/{id}', [PersonalController::class, 'update'])->name('personal.update'); //127.0.0.1:8000/personal/83
-        Route::get('/{id}/destroy', [PersonalController::class, 'destroy'])->name('personal.destroy'); ////127.0.0.1:8000/personal/83/destroy
+        Route::get('/', [PersonalController::class, 'index'])->name('personal.index');
+        Route::get('/create', [PersonalController::class, 'create'])->name('personal.create');
+        Route::post('/', [PersonalController::class, 'store'])->name('personal.store');
+        Route::get('/{id}', [PersonalController::class, 'show'])->name('personal.show');
+        Route::get('/{id}/edit', [PersonalController::class, 'edit'])->name('personal.edit');
+        Route::put('/{id}', [PersonalController::class, 'update'])->name('personal.update');
+        Route::get('/{id}/destroy', [PersonalController::class, 'destroy'])->name('personal.destroy');
     });
 
     Route::group(['prefix'=>'producto'], function (){
-        Route::get('/', [ProductoController::class, 'index'])->name('producto.index');  //127.0.0.1:8000/personal/
-        Route::get('/create', [ProductoController::class, 'create'])->name('producto.create');  //127.0.0.1:8000/personal/create
-        Route::post('/', [ProductoController::class, 'store'])->name('producto.store');  //127.0.0.1:8000/personal/
-        Route::get('/{id}', [ProductoController::class, 'show'])->name('producto.show'); //127.0.0.1:8000/personal/83
-        Route::get('/{id}/edit', [ProductoController::class, 'edit'])->name('producto.edit'); //127.0.0.1:8000/personal/83/edit
-        Route::put('/{id}', [ProductoController::class, 'update'])->name('producto.update'); //127.0.0.1:8000/personal/83
-        Route::get('/{id}/destroy', [ProductoController::class, 'destroy'])->name('producto.destroy'); ////127.0.0.1:8000/personal/83/destroy
+        Route::get('/', [ProductoController::class, 'index'])->name('producto.index');
+        Route::get('/create', [ProductoController::class, 'create'])->name('producto.create');
+        Route::post('/', [ProductoController::class, 'store'])->name('producto.store');
+        Route::get('/{id}', [ProductoController::class, 'show'])->name('producto.show');
+        Route::get('/{id}/edit', [ProductoController::class, 'edit'])->name('producto.edit');
+        Route::put('/{id}', [ProductoController::class, 'update'])->name('producto.update');
+        Route::get('/{id}/destroy', [ProductoController::class, 'destroy'])->name('producto.destroy');
     });
     
     Route::group(['prefix'=>'user'], function (){
-        Route::get('/', [UserController::class, 'index'])->name('user.index');  
+        Route::get('/', [UserController::class, 'index'])->name('user.index');
         Route::get('/create', [UserController::class, 'create'])->name('user.create');
         Route::post('/', [UserController::class, 'store'])->name('user.store');
-        Route::get('/{id}/edit', [UserController::class, 'edit'])->name('user.edit'); //127.0.0.1:8000/personal/83/edit
-        Route::put('/{id}', [UserController::class, 'update'])->name('user.update'); //127.0.0.1:8000/personal/83
-        Route::get('/{id}/destroy', [UserController::class, 'destroy'])->name('user.destroy'); ////127.0.0.1:8000/personal/83/destroy
+        Route::get('/{id}/edit', [UserController::class, 'edit'])->name('user.edit');
+        Route::put('/{id}', [UserController::class, 'update'])->name('user.update');
+        Route::get('/{id}/destroy', [UserController::class, 'destroy'])->name('user.destroy');
     });
 });
